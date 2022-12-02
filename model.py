df001dd754255d4ea7a149acc6d05cb5bf2baa51
San Kim <nasmik419@yonsei.ac.kr>
2022-12-02 11:19:08 +0900

    fix model

--- model.py
+++ model.py
@@ -3,20 +3,16 @@ import torch.nn as nn
 
 
 class OurModel(nn.Module):
-    def __init__(self, training=True):
+    def __init__(self):
         super(OurModel, self).__init__()
         self.token_birnn = nn.RNN(202, 100, bidirectional=True)
         self.tag_predict = nn.Linear(200, 19)
         self.softmax = nn.Softmax(dim=2)
         self.crf = torchcrf.CRF(19, batch_first=True)
-        self.training = training
 
-    def forward(self, x, y):
+    def forward(self, x):
         x = self.token_birnn(x)
         x = self.tag_predict(x[0])
         x = self.softmax(x)
-        if self.training:
-            x = self.crf(x, y)
-        else:
-            x = self.crf.decode(x)
+        x = self.crf.decode(x)
         return x
