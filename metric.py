812d05bdc8a71f72f68e0fbc63c2a8a5f1b47d6c
San Kim <nasmik419@yonsei.ac.kr>
2022-12-04 18:32:58 +0900

    add precision recall

--- metric.py
+++ metric.py
@@ -1,10 +1,46 @@
+import torch
 import numpy as np
 
 
 def get_accuracy(data, target):
     accuracy_list = []
     for x, y in zip(data, target):
+        x, y = x.cpu(), y.cpu()
         is_correct = (np.array(x) != -1) & (np.array(x) == np.array(y))
         is_data = np.array(y) != -1
         accuracy_list.append(len(is_correct[is_correct == True]) / len(is_data[is_data == True]))
     return accuracy_list
+
+
+def get_recall(data, target, recall_list):
+    for x, y in zip(data, target):
+        x, y = x.cpu(), y.cpu()
+        for index, label in enumerate(y):
+            if label == -1:
+                break
+            if x[index] == label:
+                recall_list[label.type(torch.int32)][0] += 1
+            else:
+                recall_list[label.type(torch.int32)][1] += 1
+    return recall_list
+
+
+def get_precision(data, target, precision_list):
+    for x, y in zip(data, target):
+        x, y = x.cpu(), y.cpu()
+        for index, label in enumerate(x):
+            if label == -1:
+                break
+            if y[index] == label:
+                precision_list[label.type(torch.int32)][0] += 1
+            else:
+                precision_list[label.type(torch.int32)][1] += 1
+    return precision_list
+
+
+def calculate(data):
+    answer = []
+    for i in data:
+        if i[0] != 0:
+            answer.append(i[0] / (i[0] + i[1]))
+    return np.mean(answer)
--- train.py
+++ train.py
@@ -1,6 +1,4 @@
-import torch
 from metric import *
-from tqdm import tqdm
 
 
 def train(model, args, dataloader):
@@ -11,7 +9,9 @@ def train(model, args, dataloader):
     for epoch in range(args.epochs):
         train_loss = []
         train_acc = []
-        for step, batch in tqdm(enumerate(dataloader)):
+        train_rec = [[0, 0] for _ in range(19)]
+        train_pre = [[0, 0] for _ in range(19)]
+        for step, batch in enumerate(dataloader):
             optimizer.zero_grad()
             batch = tuple(t.to(args.device) for t in batch)
             b_input, b_label = batch
@@ -21,11 +21,16 @@ def train(model, args, dataloader):
 
             train_loss.append(loss.item())
             train_acc.extend(get_accuracy(label, b_label))
+            train_rec = get_recall(label, b_label, train_rec)
+            train_pre = get_precision(label, b_label, train_pre)
 
         avg_train_loss = np.mean(train_loss)
         avg_train_acc = np.mean(train_acc)
-        print("Epoch {0},  Average training loss: {1:.2f},  Average training accuracy: {2:.4f}"
-              .format(epoch, avg_train_loss, avg_train_acc))
+        avg_train_rec = calculate(train_rec)
+        avg_train_pre = calculate(train_pre)
+        print("Epoch {0},  Training loss: {1:.2f},  Training accuracy: {2:.4f},  Training recall: {3:.4f},  "
+              "Training precision: {4:.4f}"
+              .format(epoch, avg_train_loss, avg_train_acc, avg_train_rec, avg_train_pre))
 
 
 def predict(model, args, dataloader):
@@ -33,7 +38,7 @@ def predict(model, args, dataloader):
     predict_labels = []
 
     print('start predicting')
-    for step, batch in tqdm(enumerate(dataloader)):
+    for step, batch in enumerate(dataloader):
         batch = tuple(t.to(args.device) for t in batch)
         with torch.no_grad():
             out = model(batch)
